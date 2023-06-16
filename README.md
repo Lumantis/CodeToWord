6814396323efae29e6be0eaa84a790d6c5c80a15
Lumantis <131993718+Lumantis@users.noreply.github.com>
2023-06-16 22:13:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+Générateur de documents de fichiers de code
+===========================================
+
+Ce programme permet de créer un document Word contenant le contenu des fichiers de code d'un répertoire sélectionné.
+
+Utilisation
+-----------
+1. Exécutez le programme en utilisant Python 3.
+2. Sélectionnez un répertoire contenant les fichiers de code que vous souhaitez inclure dans le document.
+3. Cliquez sur le bouton "Créer Document" pour générer le document Word.
+4. Choisissez l'emplacement et le nom du fichier de sortie dans la boîte de dialogue de sauvegarde.
+5. Le document Word sera créé avec succès.
+
+Prérequis
+---------
+- Python 3.x installé sur votre système.
+- Les packages suivants doivent être installés :
+  - tkinter
+  - docx
+  - tqdm
+  - ttkbootstrap
+  - Pillow
+
+Vous pouvez installer les packages requis en utilisant la commande suivante :
+
+pip install -r requirements.txt
+
+Assurez-vous d'exécuter cette commande dans un environnement virtuel ou avec les privilèges administratifs si nécessaire.
+
+Auteur
+------
+André Monteiro
---
+++ main.py
@@ -0,0 +1,100 @@
+import os
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from tqdm import tqdm
+import ttkbootstrap
+from PIL import Image, ImageTk
+
+# liste des extensions de fichiers qui vont être lu
+extensions = ['.js', '.env', '.py', '.css', '.html', '.txt', '.json', '.yaml', '.yml', '.ini']
+
+def lire_fichier(chemin):
+    try:
+        with open(chemin, "r", encoding='utf-8') as fichier:
+            return fichier.read()
+    except Exception as e:
+        messagebox.showerror("Erreur", f"Erreur lors de la lecture du fichier {chemin}: {e}")
+        return None
+
+def selectionner_repertoire():
+    repertoire = filedialog.askdirectory()
+    if repertoire:
+        app.repertoire = repertoire
+        label_repertoire["text"] = f"Répertoire sélectionné : {repertoire}"
+        bouton_creer_document["state"] = "normal"
+
+def creer_document():
+    if not app.repertoire:
+        messagebox.showinfo("Aucun répertoire sélectionné", "Veuillez d'abord sélectionner un répertoire.")
+        return
+
+    doc = Document()
+
+    # ajout d'un titre au document
+    titre = doc.add_paragraph()
+    run = titre.add_run("Fichiers de code :")
+    run.font.size = Pt(24)
+    titre.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    fichiers_a_traiter = []
+    for chemin_repertoire, noms_repertoire, noms_fichier in os.walk(app.repertoire):
+        for nom_fichier in noms_fichier:
+            if any(nom_fichier.endswith(ext) for ext in extensions):
+                chemin_fichier = os.path.join(chemin_repertoire, nom_fichier)
+                fichiers_a_traiter.append(chemin_fichier)
+
+    for chemin_fichier in tqdm(fichiers_a_traiter, desc="Traitement des fichiers", unit="fichier"):
+        contenu_fichier = lire_fichier(chemin_fichier)
+        if contenu_fichier is None:
+            continue
+
+        # ajout du nom et de l'emplacement du fichier au document
+        paragraphe = doc.add_paragraph()
+        run = paragraphe.add_run(f"Fichier : {os.path.basename(chemin_fichier)}\nEmplacement : {chemin_fichier}\n")
+        run.bold = True
+        run.italic = True
+
+        # ajout du contenu du fichier au document
+        doc.add_paragraph(contenu_fichier)
+
+    chemin_fichier = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Document Word", "*.docx")])
+    if not chemin_fichier:  # l'utilisateur a annulé la boîte de dialogue de sauvegarde
+        return
+
+    doc.save(chemin_fichier)
+    messagebox.showinfo("Succès", "Document créé avec succès !")
+
+app = ttkbootstrap.Style().master
+app.geometry("600x400")  # taille de la fenêtre
+app.title("Code To Docx")  # titre de la fenêtre
+app.repertoire = None
+
+# Chargement et placement du logo tavu
+logo = Image.open("Codetoword.png")
+logo = logo.resize((30, 30), Image.LANCZOS)
+photo_logo = ImageTk.PhotoImage(logo)
+logo_label = ttkbootstrap.Label(image=photo_logo)
+logo_label.image = photo_logo
+logo_label.pack(pady=10)
+
+label_repertoire = ttkbootstrap.Label(app, text="Aucun répertoire sélectionné")
+label_repertoire.pack(padx=10, pady=10)
+
+bouton_selectionner_repertoire = ttkbootstrap.Button(app, text="Sélectionner un répertoire", command=selectionner_repertoire)
+bouton_selectionner_repertoire.pack(padx=10, pady=10)
+
+bouton_creer_document = ttkbootstrap.Button(app, text="Créer Document", command=creer_document, state="disabled")
+bouton_creer_document.pack(padx=10, pady=10)
+
+# ajouter un menu
+menu = tk.Menu(app)
+app.config(menu=menu)
+
+menu_fichier = tk.Menu(menu)
+menu.add_cascade(label="Fichier", menu=menu_fichier)
+menu_fichier.add_command(label="Quitter", command=app.quit)
+
+app.mainloop()
